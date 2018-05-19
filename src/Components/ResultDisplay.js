674ee41982b8c91513f7021514ea676d272dd54a
brittanyjat <33502518+brittanyjat@users.noreply.github.com>
2018-05-19 14:41:41 -0600

    finshed styling user cards

--- src/Components/ResultDisplay.js
+++ src/Components/ResultDisplay.js
@@ -4,26 +4,27 @@ export default function ResultDisplay(props) {
     let display = props.results.map(user => {
         return (
             <div key={user.id} className='user-individual'>
-                <div 
-                    style={{ 
-                        backgroundImage: `url(${user.picture})` }}>
-                    {/* <img src={user.picture || userPhoto} alt='user' /> */}
+                <div
+                    className='user-image-container'
+                    style={{
+                        backgroundImage: `url(${user.picture})`
+                    }}>
                 </div>
 
-                <h3>{`${user.firstName} ${user.lastName}`}</h3>
+                <div className='name-address-container'>
+                    <h3>{`${user.firstName} ${user.lastName}`}</h3>
+                    <h4>{user.address}</h4>
+                </div>
 
-
-                <h4>{user.age}</h4>
-                <h4>{user.address}</h4>
-
-                <div className='hobby-container'>
-                    <h4>Hobbies</h4>
-                    <ul>
-                        <li>{user.interests[0] || ''}</li>
-                        <li>{user.interests[1] || ''}</li>
-                        <li>{user.interests[2] || ''}</li>
-                        <li>{user.interests[3] || ''}</li>
-                    </ul>
+                <div className='age-hobby-container'>
+                    <h5>— Age —</h5>
+                    <h4>{user.age}</h4>
+                    <hr />
+                    <h5>— Hobbies —</h5>
+                    <h4>{user.interests[0] || ''}</h4>
+                    <h4>{user.interests[1] || ''}</h4>
+                    <h4>{user.interests[2] || ''}</h4>
+                    <h4>{user.interests[3] || ''}</h4>
                 </div>
             </div>
         )
